7f1f12f623abcb98f1b9607b9716da7921dc28e6
sudeetha <sudeetha.r@gmai.com>
2024-03-06 09:42:17 +1300

    docker added

--- src/main/java/com/radium/test/controller/MusicController.java
+++ src/main/java/com/radium/test/controller/MusicController.java
@@ -3,14 +3,17 @@ package com.radium.test.controller;
 import com.radium.test.service.ArtistService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/radium")
 public class MusicController {
     private final ArtistService artistService;
     Logger log = LoggerFactory.getLogger(MusicController.class);
@@ -18,7 +21,8 @@ public class MusicController {
         this.artistService = artistService;
     }
 
-    @GetMapping(value = "/artists/mostProductive")
+    @GetMapping(path = "/artists/mostProductive",produces = {MediaType.APPLICATION_JSON_VALUE})
+
     public ResponseEntity<List<String>> mostProductiveArtists(@RequestParam(name = "topn", defaultValue = "1") Integer topn) {
         log.info("getting top {} most productive artists", topn);
         return ResponseEntity.ok(artistService.getMostProductiveArtists(topn));
